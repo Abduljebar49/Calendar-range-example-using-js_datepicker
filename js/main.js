0e5e4a288214b524d2010ab421d87f286053ea83
Abduljebar Sani <Abduljebar49@gmail.com>
2023-09-21 11:10:29 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+import datepicker from "js-datepicker";
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("inside data ")
+    const dateInput = document.getElementById("date");
+    datepicker("#date", {
+        startDay: 1,
+        showAllDates: true,
+        disableMobile: true,
+        maxDate: new Date(2020, 4, 10),
+        minDate: new Date(2020, 0, 1),
+        dateSelected: new Date(2020, 0, 5),
+        overlayPlaceholder: "4-Stelliges Jahr",
+        customMonths: [
+            "Januar",
+            "Februar",
+            "März",
+            "April",
+            "Mai",
+            "Juni",
+            "Juli",
+            "August",
+            "September",
+            "Oktober",
+            "November",
+            "Dezember"
+        ],
+        customOverlayMonths: [
+            "Jan",
+            "Feb",
+            "März",
+            "April",
+            "Mai",
+            "Juni",
+            "Juli",
+            "Aug",
+            "Sep",
+            "Okt",
+            "Nov",
+            "Dez"
+        ],
+        customDays: ["So", "Mo", "Di", "Mi", "DO", "Fr", "Sa"],
+        overlayButton: "Auswahl",
+        onSelect: (instance, date) => {
+            const value = date.toISOString().substring(0, 10);
+            console.log(date);
+            dateInput.value = value; // => '1/1/2099'
+        }
+    });
+})
